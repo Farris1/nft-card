2c210d4175a044b1cc0a04e2ebcbf0abe8a1f192
Farris1 <60323823+Farris1@users.noreply.github.com>
2022-12-03 20:02:17 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# CatStudios -  NFT preview card component solution
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+
+### Screenshot
+
+![Desktop Design](./design/desktop-design.jpg)
+![Mobile Design](./design/mobile-design.jpg)
+
+
+### Links
+
+- Live Site URL: [https://farris1.github.io/nft-card/](https://farris1.github.io/nft-card/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+
+## Author
+
+- Discord Server - [CatStudios](https://discord.gg/3Gn3DxSS3Y)
+- Discord User - [CatStudios#0975]
+- Twitter - **COMING SOON**
---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import CountUp from 'react-countup';
+
+import nftImage from "./images/image-equilibrium.jpg";
+import ethIcon from "./images/icon-ethereum.svg"
+import iconClock from "./images/icon-clock.svg"
+import avatarImage from "./images/image-avatar.png"
+import iconView from "./images/icon-view.svg"
+import "./App.css";
+
+export default function App() {
+
+  // const timer = <CountUp delay={2} end={100} />
+
+  // if (timer === 100 ) {
+  //   console.log("Okay")
+  // }
+
+  return (
+    <div className="container">
+    <div className="icon--container">
+      <img className="icon--nft" src={nftImage} alt="" />
+        <div className="middle">
+          <div className="icon--view">
+            <img src={ iconView } alt="" />
+          </div>
+        </div>
+    </div>
+      <section aria-labelledby="card">
+        <h3 className="content">
+          Equilibrium #3429
+        </h3>
+        <p className="content para"> Our Equilibrium collection promotes balance and calm.</p>
+        
+        <div className="price--content">
+          <img className="icon--price" src={ethIcon} alt="" />
+          <p className="price para">0.041 ETH</p>
+        </div>
+
+        <div className="time--content" aria-label="time">
+          <img className="icon--price" src={iconClock} alt="" />
+          <p id="timer" className="price para">
+            <CountUp 
+            delay={1} 
+            end={100} 
+            onEnd={() => 
+              document.getElementById("timer").innerHTML = "3 Days Left"}
+            />
+          </p>
+        </div>
+
+        <div id="line" aria-label="hidden"></div>
+
+        <div id="avatar--section">
+          <img className="avatar--image" src={avatarImage} alt="" />
+          <p className="avatar--text para parahov">Jules Wyvern</p>
+          <p className="avatar--text para">
+            NFT Created By
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
